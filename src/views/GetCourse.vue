64ffba9df390a13cd6c89393a9d05e77cbe011ab
ozon84 <ozon8400@gmail.com>
2019-09-10 16:35:20 +0900

    third steps

---
+++ src/views/GetCourse.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <!-- <v-btn @click="getCourseList()" color="warning">Click!</v-btn> -->
+        <v-expansion-panels>
+            <v-expansion-panel v-for="(item,idx) in courses" :key="idx">
+                <v-expansion-panel-header>
+                    <h3>{{ item.title }}</h3>
+                </v-expansion-panel-header>
+                <v-expansion-pannel-content>
+                    수강기간: {{item.duration}}<br/>
+                    최소인원: {{item.minEnrollment}}<br/>
+                    최대인원: {{item.maxEnrollment}}<br/>
+                    수 강 료: {{itme.unitPrice}}<br/>
+                    <v-card-cations>
+                        <div class="flex-grow-1">
+                        </div>
+                        <v-btn text color="warning">선택</v-btn>
+                    </v-card-cations>
+                </v-expansion-pannel-content>
+            <!-- <v-expansion-panel-conent v-for="(item,idx) in courses" :key="idx">
+                <div slot="header">
+                    <h3>{{ item.title }}</h3>
+                </div>
+                <v-card>
+                </v-card>
+            </v-expansion-panel-conent>-->
+        </v-expansion-panel> 
+        </v-expansion-panels>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            courses:[
+                {
+                    "title":"에휴 힘들다"
+                },
+                {
+                    "title":"대체 뭔소린지..."
+                }
+            ]
+        }    
+    },
+    methods:{
+        getCourseList(){
+            this.$axios
+            .get("http://13.124.69.97:8080/courses")
+            .then(res => {
+                console.log(res.data._embedded.course);
+                this.courses = res.data_embedded.course;
+            })
+            .catch(e=>this.console.log(e));
+        }
+    },
+    created(){
+        this.getCourseList();
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <HelloWorld />
+  <div class="schedule-course">
+    <hi> Make a course schedule</hi>
+    <get-course v-if="step== 1"></get-course>
+  </div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld';
+import GetCourse from './GetCourse';
 
 export default {
   components: {
-    HelloWorld,
+    GetCourse
   },
+  data() {
+    return {
+      step: 1,
+      selectedCourse: null
+    }
+  }
 };
 </script>
